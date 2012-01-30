605000c5e01b26c4359156dc889576e48d0d1b7d
Pete Doherty <ethagnawl@gmail.com>
2012-01-30 01:13:03 -0500

    adding gpl

--- fried_green_pomodoro.src.js
+++ fried_green_pomodoro.src.js
@@ -1,3 +1,26 @@
+/*
+*   Fried Green Pomodoro
+*   V1
+*   ethagnawl@gmail.com
+*   http://ethagnawl.com/pomodoro
+*   Copyright 2012, Pete Doherty
+*   Date: 1/30/2012 1:10:00 (EST)
+*
+*   Fried Green Pomodoro is distributed under the terms of the GNU General Public License.
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+*   GNU General Public License for more details.
+*
+*   http://www.gnu.org/licenses/gpl.txt
+*/
+
 (function () {
     var work_length = 25,
         ready = false,
